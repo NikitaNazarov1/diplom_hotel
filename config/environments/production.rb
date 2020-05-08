39f3b65aa531a2255055f3ed32ab866da24f0a80
NikitaNazarov1 <nikenor11@gmail.com>
2020-05-08 20:22:45 +0300

    precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,11 +20,7 @@ RubyHotel::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
-
-  # try to fix some troubles
-  config.assets.debug = true
-
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
