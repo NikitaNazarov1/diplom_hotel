8627cbddebd493345876db6946c4c959b83ded65
NikitaNazarov1 <nikenor11@gmail.com>
2020-05-17 21:59:37 +0300

    modify document types

--- app/models/passport.rb
+++ app/models/passport.rb
@@ -3,7 +3,7 @@ class Passport < ActiveRecord::Base
 
   validates :document_type, :number, :date_of_issue, :issuing_authority, presence: true
 
-  DOCUMENT_TYPES = ['Паспорт РФ', 'Заграничный паспорт', 'Иностранный документ', 'Водительское удостоверение']
+  DOCUMENT_TYPES = ['Паспорт РБ', 'Заграничный паспорт', 'Иностранный документ', 'Водительское удостоверение', 'Вид на жительство', 'Удостоверение беженца']
 
   validates :document_type, inclusion: { in: DOCUMENT_TYPES }
 end
